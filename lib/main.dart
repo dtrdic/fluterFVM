76ba05b2ea89ab9c30ea28a75e6af1b0f329f076
dtrdic <david@nevercode.io>
2023-08-08 15:24:02 +0300

    revert main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'firebase_options.dart';
 
-Future<void> main() async {
+void main() {
   runApp(const MyApp());
-  await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
-);
 }
 
 
