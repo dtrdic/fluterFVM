5c820de866d1491253d088318150bd2256fc1773
dtrdic <david@nevercode.io>
2023-08-31 13:35:29 +0200

    throw Exception("Intentional exception on startup");

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+        throw Exception("Intentional exception on startup");
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
