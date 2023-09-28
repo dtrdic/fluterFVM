f49bc7c21410f0ccfd8d7db40faa24c03a14c4b3
dtrdic <david@nevercode.io>
2023-09-28 14:20:43 +0200

    --workspace "ios/Runner.xcworkspace" \

---
+++ ios/Runner/flutter_fvmTests.swift
@@ -0,0 +1,32 @@
+//
+//  flutter_fvmTests.swift
+//  flutter_fvmTests
+//
+//  Created by David on 28.09.2023..
+//
+
+import XCTest
+
+final class flutter_fvmTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        XCTAssertEqual(1 + 1, 2)
+    }
+
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
