99d4d02dc0946267a66a204c445cba12dd5ce300
david trdic <david@nevercode.io>
2023-11-20 08:56:02 +0100

    Added a watchOS target

---
+++ ios/WatchLandmarks Watch App/WatchLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLandmarksApp.swift
+//  WatchLandmarks Watch App
+//
+//  Created by David on 20.11.2023..
+//
+
+import SwiftUI
+
+@main
+struct WatchLandmarks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
